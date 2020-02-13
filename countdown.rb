4c2bc161c8995580fe4c9e86a9a0d07c56534313
Daniel Alves de Carvalho <danielcarvalhovr@gmail.com>
2020-02-13 16:12:10 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,10 @@
 #write your code here
 
-def countdown
+def countdown(sec)
+  counter = 10
+    while countdown > 0
+      puts "#{sec} SECOND(S)!"
+      counter -+ 1
+    end
+  puts "HAPPY NEW YEAR!" 
 end
